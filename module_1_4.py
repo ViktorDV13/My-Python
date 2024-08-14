85107ecc3d6b77bc07627b2a72a31352242202b8
ViktorDV13 <95918661+ViktorDV13@users.noreply.github.com>
2024-08-14 18:27:04 +1000

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,9 +1,9 @@
 print('Hello world')
 name = "Name: Viktor Emets"
 print(name)
-age = "Age 66"
+age = "Age; 66"
 print(age)
-new_age = "NewAge 67"
+new_age = "NewAge; 67"
 print(new_age)
-IsStudent = "Ia Student True"
+IsStudent = "Is Student; True"
 print(IsStudent)
