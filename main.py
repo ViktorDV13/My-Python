52f4bc8a5bf2dca15fd293a2bb8bba78e7686f90
ViktorDV13 <95918661+ViktorDV13@users.noreply.github.com>
2024-08-14 10:29:33 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello world")
+name1 = "Курс : Python"
+name2 = 'количество операций'
+task_count = "12"
+name3 = "затраченно времени"
+hour_count = "1.5"
+name4 = "производительность"
+efficiency_count = "0.125"
+print(name1,name2 ,task_count ,name3 ,hour_count ,name4 ,efficiency_count) # умение сформулировать задачу важнее умения его выполнить
+
